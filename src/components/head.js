ccc28ab2dc43ebbb519da9fac6906813c793916b
Abdul Manan <90985527+mananabbasi51325@users.noreply.github.com>
2023-10-13 14:13:02 -0400

    Update head.js

--- src/components/head.js
+++ src/components/head.js
@@ -18,6 +18,15 @@ export default function Head({ title, description, image }) {
       <meta name="twitter:title" content={title} />
       {description && <meta name="twitter:description" content={description} />}
       {image && <meta name="twitter:image" content={image.url} />}
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-00L0DNDNVX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-00L0DNDNVX');
+</script>
     </>
   )
 }
